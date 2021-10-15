2ed039af9c8e3acd051cf368f5176b701228e748
tiagoop <90572119+tiagoop@users.noreply.github.com>
2021-10-15 14:20:14 -0300

    Initial commit

---
+++ ex008/index.js
@@ -0,0 +1,9 @@
+    var m = 5.0 
+    console.log (`A sua media ficou ${m}. `)
+
+    if (m <= 6.0) {
+        console.log (`Putz! Parece que você ficou abaixo da média mas continue se esforçando!`)
+        }
+
+    console.log ('Bons estudos!')
+    
---
+++ ex011/index.js
@@ -0,0 +1,9 @@
+var idade = 60
+
+console.log (`Você tem ${idade} anos.`)
+
+if (idade < 16 ){console.log ('Você não pode votar.')}
+
+else if (idade < 18 || idade >= 60) {console.log ('O seu voto é opcional')}
+
+else {console.log ('O seu voto é obrigatorio')}
